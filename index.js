77a8c3a4cc357b70e31cdd4616e5b5846bc32f5c
Hoang Long <trinhhoanglong200@gmail.com>
2020-12-10 10:59:13 +0700

    Handle bar

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname ));
+var hbs = require('express-handlebars');
+app.engine('hbs',hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+ 
+}));
+
+app.set('view engine','hbs');
+app.set('port',(process.env.PORT || 5000));
+
+
+
+app.get('/',function(req,res){
+    res.render('index');
+
+})
+
+// Navigation
+// var Navi =[
+//     {name: 'index',link: '/' },
+//     {name: 'recipes',link: '/recipes' },
+//     {name: 'fetured',link: '/fetured' },
+
+// ]
+
+
+
+
+//Question 1
+var recipes = require('./routes/task1')
+app.use('/recipes',recipes);
+//Question 2
+var featured = require('./routes/task2')
+app.use('/featured',featured);
+
+app.get('/videos', function(req,res){
+    res.render('videos');
+})
+app.get('/about', function(req,res){
+    res.render('about');
+})
+app.get('/blog', function(req,res){
+    res.render('blog');
+})
+
+app.listen(app.get('port'),function(){
+    console.log("Listenning "+ app.get('port'));
+});
